abf16e9c485b7fa9de0cbb35084df5904676cc1e
francoromanolosada <francoromano1994@gmail.com>
2021-04-04 22:19:59 +0200

    Added text to global styles

--- react-ui/src/styles/global-styles.js
+++ react-ui/src/styles/global-styles.js
@@ -9,6 +9,7 @@ const GlobalStyles = createGlobalStyle`
     --color-text: black;
     --color-text-inverse: white;
     --color-background: white;
+    --color-hover: hsl(6, 0%, 92%);
     --color-primary: #2F0079;
     --color-primary-rgb: 47,0,121;
     --color-secondary-rgb: 0,0,0;
@@ -18,16 +19,37 @@ const GlobalStyles = createGlobalStyle`
     --width-container: 90%;
     --width-card-frame: 17.775rem;
     --max-width-container: 40rem;
-    --spacing-xxs: .25rem;
+
+    --text-base: 1rem;
+    --text-lg: 1.125rem;
+    --text-xl: 1.25rem;
+    --text-2xl: 1.5rem;
+    --text-3xl: 1.875rem;
+    --text-4xl: 2.25rem;
+
+    --spacing-xxs: .15rem;
     --spacing-xs: .5rem;
     --spacing-s: 1rem;
     --spacing-m: 1.5rem;
     --spacing-l: 2rem;
   }
+  
   body {
     font-size: 16px;
     font-family: 'Roboto', sans-serif;
     font-weight: 400;
+  }
+
+  h1 {
+    font-size: var(--text-3xl);
+  }
+
+  h2 {
+    font-size: var(--text-2xl);
+  }
+
+  h3 {
+    font-size: var(--text-xl);
   }
 `
 
