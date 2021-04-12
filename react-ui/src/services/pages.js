dc21e76c4d858207e54a966890b3a31b0d589432
francoromanolosada <francoromano1994@gmail.com>
2021-04-12 16:48:32 +0200

    Separate initial object from the components

---
+++ react-ui/src/services/pages.js
@@ -0,0 +1,72 @@
+import { uid } from '../utilities'
+
+const pages = [
+  {
+    information: {
+      id: uid(),
+      title: 'Your first page!',
+      cover: null,
+    },
+    blocks: [
+      {
+        id: uid(),
+        html: 'First block',
+        tag: 'p',
+        placeholder: 'Type \'/\' for commands',
+      },
+      {
+        id: uid(),
+        html: 'Second block',
+        tag: 'h1',
+        placeholder: 'Heading 1',
+      },
+      {
+        id: uid(),
+        html: 'Third block',
+        tag: 'h2',
+        placeholder: 'Heading 2',
+      },
+      {
+        id: uid(),
+        html: 'Fourth block',
+        tag: 'h3',
+        placeholder: 'Heading 3',
+      },
+    ],
+  },
+  {
+    information: {
+      id: uid(),
+      title: 'Your second page!',
+      cover: null,
+    },
+    blocks: [
+      {
+        id: uid(),
+        html: 'A simple paragraph from second page',
+        tag: 'p',
+        placeholder: 'Type \'/\' for commands',
+      },
+      {
+        id: uid(),
+        html: 'A heading 1 from second page',
+        tag: 'h1',
+        placeholder: 'Heading 1',
+      },
+      {
+        id: uid(),
+        html: 'Third block',
+        tag: 'h2',
+        placeholder: 'Heading 2',
+      },
+      {
+        id: uid(),
+        html: 'Fourth block',
+        tag: 'h3',
+        placeholder: 'Heading 3',
+      },
+    ],
+  },
+]
+
+export default pages
