d84d4fccb769bf365c68375dcb448d48aebb7851
francoromanolosada <francoromano1994@gmail.com>
2021-04-13 23:39:39 +0200

    added type management

--- react-ui/src/components/blockAction.js
+++ react-ui/src/components/blockAction.js
@@ -1,23 +1,39 @@
 import React from 'react'
 import styled from 'styled-components'
-import dots, { ReactComponent as Dots } from '../images/blocks/dots-action.svg'
-import plus, { ReactComponent as Plus } from '../images/blocks/plus-action.svg'
+import { ReactComponent as SixDots } from '../images/blocks/sixdots-action.svg'
+import { ReactComponent as Plus } from '../images/blocks/plus-action.svg'
+import { ReactComponent as ThreeDots } from '../images/blocks/threedots-action.svg'
 
-const BlockAction = ({ type, onClick }) => (
-  <Wrapper id='block-action' onClick={onClick}>
-    {type === 'dots' ? (
-      <Dots fill="var(--color-gray)" width="14px" heigth="14px" />
-    ) : (
-      <Plus fill="var(--color-gray)" width="14px" heigth="14px" />
-    )}
-  </Wrapper>
-)
+const BlockAction = ({ type, color, onClick }) => {
+  if (type === 'six-dots') {
+    return (
+      <Wrapper id="block-action" onClick={onClick}>
+        <SixDots fill={color} width="14px" heigth="14px" />
+      </Wrapper>
+    )
+  } else if (type === 'three-dots') {
+    return (
+      <Wrapper id="block-action" onClick={onClick}>
+        <ThreeDots fill={color} width="14px" heigth="14px" />
+      </Wrapper>
+    )
+  } else if (type === 'plus') {
+    return (
+      <Wrapper id="block-action" onClick={onClick}>
+        <Plus fill={color} width="14px" heigth="14px" />
+      </Wrapper>
+    )
+  }
+}
 
 export default BlockAction
 
 const Wrapper = styled.div`
   width: 18px;
   height: 24px;
+
+  fill: ${p =>
+    p.color === 'clear-gray' ? 'var(--color-gray)' : 'var(--color-hover-85);'};
 
   visibility: hidden;
   display: flex;
