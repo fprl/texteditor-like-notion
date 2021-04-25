2a4980573265ec64da61667ccc8dc6d0d73ec60b
francoromanolosada <francoromano1994@gmail.com>
2021-04-25 19:31:05 +0200

    Added strikethrough and quoteicon Icons components

--- react-ui/src/components/Menus/SelectionMenu/Icon.js
+++ react-ui/src/components/Menus/SelectionMenu/Icon.js
@@ -3,18 +3,31 @@ import React from 'react'
 import FormatBoldIcon from '@material-ui/icons/FormatBold'
 import FormatItalicIcon from '@material-ui/icons/FormatItalic'
 import FormatUnderlinedIcon from '@material-ui/icons/FormatUnderlined'
+import FormatStrikethroughIcon from '@material-ui/icons/FormatStrikethrough'
+import FormatQuoteIcon from '@material-ui/icons/FormatQuote'
+
+const BOLD = 'bold'
+const ITALIC = 'italic'
+const UNDERLINED = 'underlined'
+const STRIKE = 'strikethrough'
+const QUOTE = 'block-quote'
 
 const Icon = ({ active, icon }) => {
+
   const style = active
     ? { color: 'var(--color-status-active)', fontSize: 'var(--text-base)' }
     : { color: 'var(--color-text)', fontSize: 'var(--text-base)' }
 
-  if (icon === 'format_bold')
+  if (icon === BOLD)
     return <FormatBoldIcon fontSize="small" style={style} />
-  if (icon === 'format_italic')
+  if (icon === ITALIC)
     return <FormatItalicIcon fontSize="small" style={style} />
-  if (icon === 'format_underlined')
+  if (icon === UNDERLINED)
     return <FormatUnderlinedIcon fontSize="small" style={style} />
+  if (icon === STRIKE)
+    return <FormatStrikethroughIcon fontSize="small" style={style} />
+  if (icon === QUOTE)
+    return <FormatQuoteIcon fontSize="small" style={style} />
 }
 
 export default Icon
